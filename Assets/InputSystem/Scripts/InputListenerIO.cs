b21463ddde4d1dd71cd5187b96754aec0c717e7d
Skibitsky <gl.skib@gmail.com>
2017-04-30 21:16:28 +0300

    Handler saving reworked

--- Assets/InputSystem/Scripts/InputListenerIO.cs
+++ Assets/InputSystem/Scripts/InputListenerIO.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
 using UnityEngine;
@@ -8,17 +9,7 @@ namespace Salday.GameFramework.InputSystem
     public class InputListenerIO
     {
         //(2017-04-26)
-        //TODO reading from file
-
-        //public static InputListener ReadHandler(string name)
-        //{
-        //    XmlSerializer xs = new XmlSerializer(typeof(InputListener));
-
-        //    using (var sr = new StreamReader(string.Format(@"c:\temp\{0}.xml", name)))
-        //    {
-        //        return xs.Deserialize(sr) as InputListener;
-        //    }
-        //}
+        //TODO reading from the file.
 
         /// <summary>
         /// Saves gived handler to the XML into _Data/Xml/InputSettings.
@@ -30,7 +21,7 @@ namespace Salday.GameFramework.InputSystem
 
             var path = Path.Combine(Application.dataPath, folder);
 
-            DirectoryInfo folderInf = new DirectoryInfo(path);
+            var folderInf = new DirectoryInfo(path);
 
             if (!Directory.Exists(path))
             {
@@ -41,16 +32,53 @@ namespace Salday.GameFramework.InputSystem
 
             using (var sw = new StreamWriter(filePath))
             {
-                XmlSerializer serializer = new XmlSerializer(typeof(InputListener));
-                foreach (var il in handler.Pressed.Values)
-                {
-                    serializer.Serialize(sw, il);
-                }
+                var serializer = new XmlSerializer(typeof(SavingHandler));
+
+                var sh = new SavingHandler(handler.JustPressed, handler.Pressed, handler.JustReleased);
+
+                serializer.Serialize(sw, sh);
 
                 sw.Close();
             }
 
         }
 
+    }
+
+
+    // We need this class to serialize it. 
+    // We can't serialize dictionaries from the handler that's why we're converting them to the lists
+    [Serializable]
+    public class SavingHandler
+    {
+        [XmlArray("JustPressed")]
+        public List<InputListener> JustPressed = new List<InputListener>();
+        [XmlArray("Pressed")]
+        public List<InputListener> Pressed = new List<InputListener>();
+        [XmlArray("JustReleased")]
+        public List<InputListener> JustReleased = new List<InputListener>();
+
+        /// <summary>
+        /// Dont't use it. It's required by XmlSerializer.
+        /// </summary>
+        public SavingHandler()
+        {
+
+        }
+
+        public SavingHandler(Dictionary<KeyCode, InputListener> justPressed,
+            Dictionary<KeyCode, InputListener> pressed,
+            Dictionary<KeyCode, InputListener> justReleased
+            )
+        {
+            foreach (var item in justPressed)
+                JustPressed.Add(item.Value);
+
+            foreach (var item in pressed)
+                Pressed.Add(item.Value);
+
+            foreach (var item in justReleased)
+                JustReleased.Add(item.Value);
+        }
     }
 }
